9b21e2726b3fcd921ed1b196e9fc796d023c8f54
naveenjpr <naveensainijpr@gmail.com>
2026-04-06 12:56:05 +0530

    port change

--- backend/src/config/mailConfig.js
+++ backend/src/config/mailConfig.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for port 465, false for other ports
+    port: 587,            // ✅ change here
+    secure: false,        // ✅ important (false for 587)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
