67dfd5c1f271dc896f1a2c4f9d50870a0edfd10e
naveenjpr <naveensainijpr@gmail.com>
2026-04-07 11:43:36 +0530

    hello

--- backend/src/controllers/website/SociallyEngaged.controller.js
+++ backend/src/controllers/website/SociallyEngaged.controller.js
@@ -1,26 +1,21 @@
 const Social = require("../../models/SociallyEngaged.Schema");
 
-
 // 👉 VIEW ALL Social
 exports.view = async (req, res) => {
-
-    try {
-        const projects = await Social.find({ status: true }).sort({
-            created_at: -1,
-        });
-        res.status(200).json({
-            status: true,
-            message: "Social fetched successfully",
-            totalRecords,
-            data: projects,
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: false,
-            message: "Failed to fetch Social",
-            error: err.message,
-        });
-    }
+  try {
+    const projects = await Social.find({ status: true }).sort({
+      created_at: -1,
+    });
+    res.status(200).json({
+      status: true,
+      message: "Social fetched successfully",
+      data: projects,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: "Failed to fetch Social",
+      error: err.message,
+    });
+  }
 };
-
-
--- backend/src/routes/website/SociallyEngaged.routes.js
+++ backend/src/routes/website/SociallyEngaged.routes.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const route = express.Router();
 const SocialController = require("../../controllers/website/SociallyEngaged.controller");
-const upload = require("../../config/upload");
 
 module.exports = (app) => {
-    route.post("/view", SocialController.view);
+  route.post("/view", SocialController.view);
 
-
-    app.use("/api/website/Social", route);
+  app.use("/api/website/Social", route);
 };
 //http://localhost:5000/api/website/Social/view
-
